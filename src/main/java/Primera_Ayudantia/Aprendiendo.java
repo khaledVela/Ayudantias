0511385437169ffab9b9620d06cc46a0297c1eba
khaledVela <80561953+khaledVela@users.noreply.github.com>
2021-05-04 01:13:57 -0400

    Repositorio para las ayudantias

---
+++ src/main/java/Primera_Ayudantia/Aprendiendo.java
@@ -0,0 +1,10 @@
+package Primera_Ayudantia;
+
+public class Aprendiendo {
+    int Numero;
+    String Letra;
+    boolean Flag;
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
